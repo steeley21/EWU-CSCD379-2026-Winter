fe1dd0b0a6df6eca3c6fcd49711a495db1993360
Joshua Lester <jlester3@ewu.edu>
2026-01-06 16:20:43 -0800

    class1

---
+++ wordle.web/app/app.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-container>
+        <v-btn variant="flat" color="primary" @click="increment">Hello {{ count }}</v-btn>
+    </v-container>
+</template>
+
+<script setup lang="ts">
+console.log("hi!!");
+
+let count = ref(0);
+console.log(count.value)
+
+function increment() {
+    count.value++;
+    console.log(count.value);
+}
+</script>
