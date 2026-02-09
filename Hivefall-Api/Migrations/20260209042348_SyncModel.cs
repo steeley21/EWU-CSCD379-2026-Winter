496cbaad5e576b139a25e5db36126ad8445081be
steeley21 <ksteele5@ewu.edu>
2026-02-08 20:24:46 -0800

    migrations db fix

---
+++ Hivefall-Api/Migrations/20260209042348_SyncModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hivefall_Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
