c400d94ddd3ec73272b7e9038030a3505b652e26
steeley21 <ksteele5@ewu.edu>
2026-02-08 23:20:54 -0800

    added favicon

--- hivefall-game/nuxt.config.ts
+++ hivefall-game/nuxt.config.ts
@@ -2,6 +2,14 @@
 import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', href: '/favicon.ico' },
+      ],
+    },
+  },
+
   runtimeConfig: {
     public: {
       /**
