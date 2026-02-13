04da057a058a94e4af466afc367ce908ada09fb6
steeley21 <ksteele5@ewu.edu>
2026-02-12 21:44:25 -0800

    main page scroll fix

--- hivefall-game/layouts/default.vue
+++ hivefall-game/layouts/default.vue
@@ -1,6 +1,7 @@
+<!-- layouts/default.vue -->
 <template>
   <v-layout>
-    <v-main>
+    <v-main class="hf-main">
       <AppHeader />
       <slot />
     </v-main>
@@ -10,3 +11,10 @@
 <script setup lang="ts">
 import AppHeader from '~/components/AppHeader.vue'
 </script>
+
+<style scoped>
+.hf-main {
+  height: 100dvh;
+  overflow-y: auto;
+}
+</style>
