e91776a18dcb8b86e23f021521e927398cb5cebc
ColtonKnopik <ColtonKnopik@gmail.com>
2026-02-20 16:28:10 -0800

    move plugins folder

--- Bookclub.Web/nuxt.config.ts
+++ Bookclub.Web/nuxt.config.ts
@@ -1,7 +1,9 @@
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  ssr: false,
+    devtools: { enabled: true },
+    plugins: ['~/plugins/vuetify.ts'],
 
   css: ['vuetify/styles', '@mdi/font/css/materialdesignicons.css'],
 
